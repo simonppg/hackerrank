077b2f4f03fd7355700304342a21212fd445e3c1
simonppg <simonppg@gmail.com>
2019-05-21 11:45:26 -0700

    Solving functions in c++

---
+++ c++/functions_in_c++.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int max_of_four(int a, int b, int c, int d) {
+    int ret = a;
+    if(b > ret)
+        ret = b;
+    if(c > ret)
+        ret = c;
+    if(d > ret)
+        ret = d;
+    return ret;
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;   
+}
+
+
