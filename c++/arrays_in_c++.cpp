26ada5c3a720fa89822a7373259e40a49e17f464
simonppg <simonppg@gmail.com>
2019-05-21 12:07:07 -0700

    Solving arrays in c++

---
+++ c++/arrays_in_c++.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n = 0;
+    scanf("%d", &n);
+
+    int *array = (int *) malloc(sizeof(int) * n);
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d ", &array[i]);
+    }
+
+    for(int i = n - 1; i >= 0;  i--) {
+        printf("%d ", array[i]);
+    }
+    return 0;
+}
